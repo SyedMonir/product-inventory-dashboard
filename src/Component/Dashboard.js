7e7aa98aee55f0ed4e0b380757a9e7c7f86563de
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-06-23 00:11:59 +0600

    added product

--- src/Component/Dashboard.js
+++ src/Component/Dashboard.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Product from './Product';
 
 const Dashboard = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getProduct() {
+      try {
+        const response = await axios.get(
+          `https://fec-inventory-api.herokuapp.com/product-info`
+        );
+        setProducts(response?.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getProduct();
+  }, []);
+
+  console.log(products[1]);
+
   return (
     <>
       <h1>Dashboard Supply Room</h1>
-      <section className="flex">
-        <div className="w-3/5">
-          <Product />
+      <section className="flex sm:flex-row flex-col">
+        <div className="sm:w-3/5 w-full gap-y-4 mx-4 grid sm:grid-cols-2">
+          {products?.map((product) => (
+            <Product key={product?.id} product={product} />
+          ))}
         </div>
-        <div className="w-2/5">h1</div>
+        <div className="sm:w-2/5 w-full">h1</div>
       </section>
     </>
   );
--- src/Component/Product.js
+++ src/Component/Product.js
@@ -1,20 +1,27 @@
 import React from 'react';
 
-const Product = () => {
+const Product = ({ product }) => {
+  const { description, name } = product;
   return (
     <>
-      <figure className="flex">
-        <div className="w-1/4">
-          <img src="" alt="" />
+      <figure className="w-auto flex  bg-slate-100 p-4 sm:mr-4">
+        <div className="sm:w-1/4 w-auto sm:mr-0 mr-4">
+          <div className="bg-red-600 text-white w-full h-full flex justify-center items-center">
+            150 X 150
+          </div>
         </div>
-        <div className="w-3/4">
-          <h2>Name</h2>
-          <div className="flex justify-between">
-            <p>Description</p>
+        <div className="sm:w-3/4 w-auto">
+          <h2>{name}</h2>
+          <div className="flex sm:flex-row flex-col justify-between">
+            <p className="text-xs">{description}</p>
 
-            <div>
-              <button>Add to List</button>
-              <button>Details</button>
+            <div className="text-sm">
+              <button className="bg-slate-200 rounded p-1 px-3 m-2">
+                Add to List
+              </button>
+              <button className="bg-slate-200 rounded p-1 px-3 m-2">
+                Details
+              </button>
             </div>
           </div>
         </div>
