d6038f7313ecb1c6cd4d9d05741619ee26a6c73f
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-06-23 11:33:08 +0600

    responsive done for the page

--- src/Component/Dashboard.js
+++ src/Component/Dashboard.js
@@ -24,13 +24,13 @@ const Dashboard = () => {
   return (
     <>
       <h1>Dashboard Supply Room</h1>
-      <section className="flex sm:flex-row flex-col">
-        <div className="sm:w-3/5 w-full gap-y-4 mx-4 grid sm:grid-cols-2">
+      <section className="flex sm:flex-row flex-col-reverse">
+        <div className="sm:w-3/5 w-auto gap-y-4 mx-4 grid sm:grid-cols-2">
           {products?.map((product) => (
             <Product key={product?.id} product={product} />
           ))}
         </div>
-        <div className="sm:w-2/5 w-full">h1</div>
+        <div className="sm:w-2/5 w-auto mx-4 bg-slate-100 mb-4">h1</div>
       </section>
     </>
   );
--- src/Component/Product.js
+++ src/Component/Product.js
@@ -4,26 +4,21 @@ const Product = ({ product }) => {
   const { description, name } = product;
   return (
     <>
-      <figure className="w-auto flex  bg-slate-100 p-4 sm:mr-4">
-        <div className="sm:w-1/4 w-auto sm:mr-0 mr-4">
-          <div className="bg-red-600 text-white w-full h-full flex justify-center items-center">
-            150 X 150
-          </div>
+      <figure className="flex bg-slate-100 p-4 sm:mr-4">
+        <div className="w-[30%] bg-red-600 text-white flex justify-center items-center">
+          150 X 150
         </div>
-        <div className="sm:w-3/4 w-auto">
+        <div className="w-2/5 mx-2 ">
           <h2>{name}</h2>
           <div className="flex sm:flex-row flex-col justify-between">
-            <p className="text-xs">{description}</p>
-
-            <div className="text-sm">
-              <button className="bg-slate-200 rounded p-1 px-3 m-2">
-                Add to List
-              </button>
-              <button className="bg-slate-200 rounded p-1 px-3 m-2">
-                Details
-              </button>
-            </div>
+            <p className="text-xs">{description.slice(0, 50)}</p>
           </div>
+        </div>
+        <div className="text-xs w-[30%] flex flex-col justify-center ">
+          <button className="bg-slate-200 rounded p-1 px-3 m-2">
+            Add to List
+          </button>
+          <button className="bg-slate-200 rounded p-1 px-3 m-2">Details</button>
         </div>
       </figure>
     </>
